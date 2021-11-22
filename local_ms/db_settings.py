f4e2a4e6c82c20091e583681f30ccf0241582252
mattsmart <mattsmart@hotmail.ca>
2021-11-22 16:07:27 -0500

    update db tickers; add label mapper in db_settings

--- local_ms/db_settings.py
+++ local_ms/db_settings.py
@@ -1,4 +1,14 @@
 DB_PATH = 'database_yfinance.db'
 
 # list of ticker strings corresponding to yahoo finance tickers (to generate database)
-DB_TICKERS = ['MSFT', 'AAPL', 'HUT', 'HUT.TO', 'SPY']
+DB_TICKERS = ['MSFT', 'AAPL', 'HUT', 'HUT.TO', 'SPY', 'CADUSD=X', 'BTC-USD', 'ETH-USD', 'ETHX-U.TO']
+
+DB_TZ = 'US/Eastern'
+
+MAP_COLUMN_TO_LABEL = {
+    'open': 'Price (open)',
+    'high': 'Price (high)',
+    'low': 'Price (low)',
+    'close': 'Price (close)',
+    'volume': 'Volume (units)',
+}
